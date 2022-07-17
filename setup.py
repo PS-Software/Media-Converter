700ad7e10b4b008387711bfc6da05ebcfa1c7290
BasicCorruption <saytleader@outlook.com>
2022-07-17 19:03:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from curses import window
+import os
 from setuptools import setup
 import py2exe
 
@@ -12,4 +13,7 @@ setup(
     console=["runner.py"],
     windows=[{"script": "main.py", "icon_resources": [(1, "assets/icon.ico")]}],
     download_url = "",
+    icon = "assets/icon.ico",
 )
+os.system("delete .\\dist\\PSMediaConverter.exe")
+os.system("rename .\\dist\\main.exe PSMediaConverter.exe")
