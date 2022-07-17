196b98d7b7e91a7acb81cbe9332ce34f8f367657
BasicCorruption <saytleader@outlook.com>
2022-07-16 20:14:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkm
 import os
 
-wndw = tk.Tk()
+global tasks
+tasks = []
+global videoformats
+videoformats = ["mp4", "avi", "mkv", "mov", "wmv", "flv", "mpg", "mpeg", "mts", "gif"]
+global audioformats
+audioformats = ["mp3", "wav", "flac", "ogg", "aac", "wma", "m4a"]
+global rasterimageformats
+rasterimageformats = ["jpg", "jpeg", "jfif", "gif", "png", "bmp", "tiff", "tif", "heic", "webp"]
+global vectorimageformats
+vectorimageformats = ["svg"]
+
+window = tk.Tk()
 #make the window size 50% screen resolution
-wndw.geometry("%dx%d+%d+%d" % (wndw.winfo_screenwidth()/2, wndw.winfo_screenheight()/2, wndw.winfo_screenwidth()/4, wndw.winfo_screenheight()/4))
+window.geometry("%dx%d+%d+%d" % (window.winfo_screenwidth()/2, window.winfo_screenheight()/2, window.winfo_screenwidth()/4, window.winfo_screenheight()/4))
 #maximize the window
-#wndw.state('zoomed')
-wndw.title("PizzaSoft Media Converter v22.0.1")
+#window.state('zoomed')
+window.title("PizzaSoft Media Converter v22.0.1")
 #set icon to icon.png
 try:
     img = tk.Image("photo", file="./assets/icon.png")
-    wndw.iconphoto(False, img)
+    window.iconphoto(False, img)
 except:
     pass
 
 style = ttk.Style()
 styles = style.theme_names()
-menu = tk.Menu(wndw)
+menu = tk.Menu(window)
 
 fileMenu = tk.Menu(menu, tearoff=0)
 fileMenu.add_command(label="Open")
 fileMenu.add_separator()
-fileMenu.add_command(label="Exit", command=wndw.quit)
+fileMenu.add_command(label="Exit", command=window.quit)
 
 tasksMenu = tk.Menu(menu, tearoff=0)
 tasksMenu.add_command(label="Run All Tasks", state="disabled")
-tasksMenu.add_command(label="Pause All Tasks", state="disabled")
-tasksMenu.add_command(label="Resume All Tasks", state="disabled")
 tasksMenu.add_command(label="Cancel All Tasks", state="disabled")
 tasksMenu.add_command(label="Remove All Tasks", state="disabled")
 
 themesSub = tk.Menu(tasksMenu, tearoff=0)
 #add all available themes to the themes submenu
 for theme in styles:
-    themesSub.add_command(label=theme, command=lambda: style.theme_use(theme))
+    themesSub.add_command(label=theme, command=lambda theme=theme: style.theme_use(theme))
 
 viewMenu = tk.Menu(menu, tearoff=0)
 viewMenu.add_cascade(label="Theme", menu=themesSub)
@@ -42,14 +53,42 @@ viewMenu.add_cascade(label="Theme", menu=themesSub)
 editMenu = tk.Menu(menu, tearoff=0)
 editMenu.add_command(label="Preferences")
 
-wndw.config(menu=menu)
+window.config(menu=menu)
 menu.add_cascade(label="File", menu=fileMenu)
 menu.add_cascade(label="Edit", menu=editMenu)
 menu.add_cascade(label="Tasks", menu=tasksMenu)
 menu.add_cascade(label="View", menu=viewMenu)
-menu.add("command", label="Help", command=lambda: os.startfile("https://ps-software.github.io/helpcenter/"))
+menu.add("command", label="Help", command=lambda: os.startfile("https://pizzasoft.us/helpcenter/"))
 
-button = ttk.Button(wndw, text="test")
-button.place(relx=0.5, rely=0.5, anchor="center")
+#make a frame for the buttons at the top
+buttons = tk.Frame(window)
+buttons.pack(side="top", fill="x")
 
-wndw.mainloop()
+def import_file():
+    #open a file dialog and get the file
+    file = tkfd.askopenfilename(filetypes=[("Video Files", videoformats), ("Audio Files", audioformats), ("Raster Image Files", rasterimageformats), ("Vector Image Files", vectorimageformats)])
+    #if the file is not empty, add it to the list
+    if file != "":
+        return file
+
+def add_task():
+    file = import_file()
+
+#make the buttons
+importButton = ttk.Button(buttons, text="Import", command=add_task)
+
+rat = ttk.Button(buttons, text="Run All Tasks", state="disabled") #you can tell who made this variable name (it was me, lewolfyt)
+cat = ttk.Button(buttons, text="Cancel All Tasks", state="disabled")
+rmat = ttk.Button(buttons, text="Remove All Tasks", state="disabled")
+
+importButton.pack(side="left")
+rat.pack(side="left")
+cat.pack(side="left")
+rmat.pack(side="left")
+
+#make the task list frame
+tasklframe = tk.Frame(window)
+tasklframe.pack(side="top", fill="both", expand=True)
+tasklist = tk.Listbox(tasklframe, selectmode="extended")
+
+window.mainloop()
