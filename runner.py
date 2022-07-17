7f37d758e7bf8a37b19eabebc10b0ed467b37851
BasicCorruption <saytleader@outlook.com>
2022-07-17 17:20:35 -0400

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,9 @@
-import ffmpeg
+import os
 
-class Runner:
-    def fRunFileExtensionConversionTask(inputFilePath, outputFileDirectory, inputFileExtensionType, outputFileExtensionType):
-        
-        return
+def fileConversionTask(inputPath, outputDirectory, inputExtension, outputExtension):
+    #we will be using ffmpeg with the os module to convert the file
+    os.popen("ffmpeg -i " + inputPath + inputExtension + outputDirectory + outputExtension)
+    return
 
-def convertTask(ip, od, it, ot):
-    return Runner.fRunFileExtensionConversionTask(ip, od, it, ot)
+def convertTask(inPath, outDir, inExt, outExt):
+    return fileConversionTask(inPath, outDir, inExt, outExt)
