def270748980d6e02005c9459342f367a40d9dbe
BasicCorruption <saytleader@outlook.com>
2022-07-17 16:59:00 -0400

    Create runner.py

---
+++ runner.py
@@ -0,0 +1,5 @@
+def fRunFileExtensionConversionTask(inputFilePath, outputFileDirectory, inputFileExtensionType, outputFileExtensionType):
+    return;
+
+def convertTask(ip, od, it, ot):
+    return fRunFileExtensionConversionTask(ip, od, it, ot)
